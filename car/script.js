63881434854c4683731ce983d83199ee370407b1
RyPaert <158561974+RyPaert@users.noreply.github.com>
2025-09-09 13:25:04 +0300

    done

--- car/script.js
+++ car/script.js
@@ -2,7 +2,14 @@ const myJson = [
     {
       Car: {
         Color: "Rose red",
-        "Tinted windows": true
+        "Tinted windows": false,
+        "Wheels": 4,
+        "Roof cargo": null,
+        Entertainment: [
+            "FM Radio",
+            "Apple CarPlay/Android Auto",
+            "Bowers & Wilkins Premium Sound speakers"
+        ]        
       }
     }
   ];
@@ -11,7 +18,9 @@ const myJson = [
     <h1>Car properties</h1>
     <p>Color: ${myJson[0].Car.Color}</p>
     <p>Tinted windows: ${myJson[0].Car['Tinted windows'] ? 'Yes' : 'No'}</p>
-    <p
+    <p>Wheels: ${myJson[0].Car.Wheels}</p>
+    <p>Roof Cargo: ${myJson[0].Car["Roof cargo"]? 'null' : 'thule'}</p>
+    <p>Entertainment: ${myJson[0].Car.Entertainment}</p>
   </div>
   `;
   
